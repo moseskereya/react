04c0129b17fbfac81e3c7f5fef74c253fb487bfb
MOSES <MOSESLAZARO2019@outlook.com>
2019-08-31 21:24:56 +0300

    Updates

---
+++ precache-manifest.351e47f4dcb2f3a45e90071483ab2d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b8bd1050cb17ed6311af9c35b5d3f1",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3aa5c975797d6d893272",
+    "url": "/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "51177cd0d14eae4d91e5",
+    "url": "/react/static/js/2.80f9c3ce.chunk.js"
+  },
+  {
+    "revision": "3aa5c975797d6d893272",
+    "url": "/react/static/js/main.1456d422.chunk.js"
+  },
+  {
+    "revision": "92bc80e21e155212a4c0",
+    "url": "/react/static/js/runtime~main.9ab412ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
